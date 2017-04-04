f23a0d4a6d90888d9c4150d16ea362fceabc3c45
Xin,Yaping <yaping.xin@honeywell.com>
2017-04-04 19:17:13 +0800

    example code

--- TechResearch/Excel-Spreadsheet/CecilHarvey-Libxls/TranslateConfiguration/TranslateConfig-ExcelReader.cpp
+++ TechResearch/Excel-Spreadsheet/CecilHarvey-Libxls/TranslateConfiguration/TranslateConfig-ExcelReader.cpp
@@ -20,6 +20,22 @@ int main()
 	FILE *f;
 	unsigned int i;
 
+	int sheet_count = 0;
+
+	struct st_row_data* row;
+	WORD t, tt;
+	pWB = xls_open("C:\\SVN\\CH5ODT4YZWKF2\\MLAPEX\\trunk\\Dev_MLAPEX\\Pretreatment\\Messages\\Messages.xls", "UTF-16");
+
+	if (pWB != NULL)
+	{
+		sheet_count = pWB->sheets.count;
+		if (sheet_count > 0)
+		{
+			pWS = xls_getWorkSheet(pWB, 0);
+			xls_parseWorkSheet(pWS);
+		}
+	}
+
     return 0;
 }
 
